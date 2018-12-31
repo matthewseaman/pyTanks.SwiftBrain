c4518463674f295bba04baf1c36899e5fc9e9c33
Matthew Seaman <mattsdeveloper@gmail.com>
2018-12-31 00:35:24 -0500

    Algorithm-agnostic Obstacle

--- Sources/Navigate/Obstacle.swift
+++ Sources/Navigate/Obstacle.swift
@@ -5,4 +5,18 @@
 //  Created by Matthew Seaman on 12/31/18.
 //
 
-import Foundation
+import CoreGraphics
+
+public struct Obstacle {
+    
+    let rect: CGRect
+    
+    public init(rect: CGRect) {
+        self.rect = rect
+    }
+    
+    func intersects(_ other: CGRect) {
+        self.rect.intersects(other)
+    }
+    
+}
