b6ae406f6636f43f9ce2d1d49acaf1dc9bee2731
Matthew Seaman <mattsdeveloper@gmail.com>
2018-12-30 23:57:10 -0500

    Use correct SwiftPlayer dependency targets

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ let package = Package(
     name: "pyTanks.SwiftBrain",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "runSample",
+            targets: ["runSample"]),
         .library(
             name: "PyBrain",
             targets: ["Brain"]),
@@ -18,11 +21,17 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
+            name: "runSample",
+            dependencies: ["AdvancedPlayer", "ClientControl"]),
+        .target(
+            name: "AdvancedPlayer",
+            dependencies: ["Brain", "PlayerSupport"]),
+        .target(
             name: "Brain",
-            dependencies: ["Navigation", "Artillery", "PyPlayer"]),
+            dependencies: ["Navigation", "Artillery", "PlayerSupport"]),
         .target(
             name: "Artillery",
-            dependencies: ["Navigation", "PyPlayer"]),
+            dependencies: ["Navigation", "PlayerSupport"]),
         .target(
             name: "Navigation",
             dependencies: ["Compute"]),
